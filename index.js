afbc24fced2da77e7bef103319b710b2a67b63a7
Fatema Abdulhakeem <fatema.hakeem.005@gmail.com>
2026-03-22 20:33:25 +0300

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @returns {number} The sum of a and b.
  */
 function sum(a, b) {
-  // TODO: Implement this function.
+  return a + b;
 }
 
 /**
@@ -12,7 +12,7 @@ function sum(a, b) {
  * @returns {string} The reversed string.
  */
 function reverseString(str) {
-  // TODO: Implement this function.
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -20,7 +20,15 @@ function reverseString(str) {
  * @returns {number} The largest number in the array. Return null if the array is empty.
  */
 function findLargest(numbers) {
-  // TODO: Implement this function.
+  if (numbers.length===0)
+    return null;
+
+  let max =numbers[0];
+  for(let num of numbers){
+    if(num>max)
+      max=num;
+  }
+  return max
 }
 
 /**
@@ -30,7 +38,10 @@ function findLargest(numbers) {
  * The check should be case-insensitive.
  */
 function isPalindrome(str) {
-  // TODO: Implement this function.
+ let norm= str.toLowerCase();
+  let rev= str.split('').reverse().join('');
+  rev=rev.toLocaleLowerCase();
+  return norm === rev;
 }
 
 /**
@@ -38,7 +49,13 @@ function isPalindrome(str) {
  * @returns {number[]} A new array containing only the even numbers from the original array.
  */
 function filterEvenNumbers(numbers) {
-  // TODO: Implement this function.
+  let result=[];
+
+  for(num of numbers){
+    if(num%2===0)
+      result.push(num);
+  }
+  return result;
 }
 
 
